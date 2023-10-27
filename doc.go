26469370458a7d75ceb3fde020ad2b9646f65430
hermannm <hermann.morkrid@gmail.com>
2023-10-27 18:31:51 +0200

    add note on devlog/log subpackage to doc.go and README

--- doc.go
+++ doc.go
@@ -6,15 +6,18 @@
 //	logger := slog.New(devlog.NewHandler(os.Stdout, nil))
 //	slog.SetDefault(logger)
 //
-// Following calls to slog's logging functions will use this handler, giving output on the following
-// format:
+// Following calls to [log/slog]'s logging functions will use this handler, giving output on the
+// following format:
 //
 //	slog.Info("Server started", slog.Int("port", 8000), slog.String("environment", "DEV"))
 //	// [2023-10-21 10:31:09] INFO: Server started
 //	// - port: 8000
 //	// - environment: DEV
 //
-// To see the output format with colors, check the [README].
+// Check the [README] to see the output format with colors.
+//
+// To complement devlog's output handling, the devlog/log subpackage provides input handling. It is
+// a thin wrapper over the slog package, with utility functions for log message formatting.
 //
 // [README]: https://github.com/hermannm/devlog#readme
 package devlog
