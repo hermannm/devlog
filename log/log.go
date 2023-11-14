8a25787c3654bba4f9024c8375f86035fe0ae35f
hermannm <hermann.morkrid@gmail.com>
2023-11-14 08:38:48 +0100

    add new Logger type to devlog/log

--- log/log.go
+++ log/log.go
@@ -17,7 +17,7 @@ import (
 // Info logs the given message at the INFO log level, along with any given structured log
 // attributes.
 func Info(message string, attributes ...slog.Attr) {
-	if logger, enabled := getLogger(slog.LevelInfo); enabled {
+	if logger, enabled := defaultLogger(slog.LevelInfo); enabled {
 		logger.log(message, attributes...)
 	}
 }
@@ -25,7 +25,7 @@ func Info(message string, attributes ...slog.Attr) {
 // Infof creates a message from the given format and arguments using [fmt.Sprintf], and logs it at
 // the INFO log level.
 func Infof(messageFormat string, formatArgs ...any) {
-	if logger, enabled := getLogger(slog.LevelInfo); enabled {
+	if logger, enabled := defaultLogger(slog.LevelInfo); enabled {
 		logger.log(fmt.Sprintf(messageFormat, formatArgs...))
 	}
 }
@@ -33,7 +33,7 @@ func Infof(messageFormat string, formatArgs ...any) {
 // Warn logs the given message at the WARN log level, along with any given structured log
 // attributes.
 func Warn(message string, attributes ...slog.Attr) {
-	if logger, enabled := getLogger(slog.LevelWarn); enabled {
+	if logger, enabled := defaultLogger(slog.LevelWarn); enabled {
 		logger.log(message, attributes...)
 	}
 }
@@ -41,7 +41,7 @@ func Warn(message string, attributes ...slog.Attr) {
 // Warnf creates a message from the given format and arguments using [fmt.Sprintf], and logs it at
 // the WARN log level.
 func Warnf(messageFormat string, formatArgs ...any) {
-	if logger, enabled := getLogger(slog.LevelWarn); enabled {
+	if logger, enabled := defaultLogger(slog.LevelWarn); enabled {
 		logger.log(fmt.Sprintf(messageFormat, formatArgs...))
 	}
 }
@@ -51,24 +51,25 @@ func Warnf(messageFormat string, formatArgs ...any) {
 //
 // If message is not blank, wraps the error with the given message using [hermannm.dev/wrap.Error].
 func WarnError(err error, message string, attributes ...slog.Attr) {
-	if logger, enabled := getLogger(slog.LevelWarn); enabled {
-		logger.log(buildErrorString(err, message), attributes...)
+	if logger, enabled := defaultLogger(slog.LevelWarn); enabled {
+		message, attributes = buildErrorLog(err, message, attributes)
+		logger.log(message, attributes...)
 	}
 }
 
 // WarnErrorf wraps the given error with a formatted message using [hermannm.dev/wrap.Errorf], and
 // logs it at the WARN log level.
 func WarnErrorf(err error, messageFormat string, formatArgs ...any) {
-	if logger, enabled := getLogger(slog.LevelWarn); enabled {
-		logger.log(buildWrappedErrorString(err, fmt.Sprintf(messageFormat, formatArgs...)))
+	if logger, enabled := defaultLogger(slog.LevelWarn); enabled {
+		logger.log(fmt.Sprintf(messageFormat, formatArgs...), errorCauseAttribute(err))
 	}
 }
 
 // WarnErrors wraps the given errors with a message using [hermannm.dev/wrap.Errors], and logs it at
 // the WARN log level.
 func WarnErrors(message string, errs ...error) {
-	if logger, enabled := getLogger(slog.LevelWarn); enabled {
-		logger.log(buildWrappedErrorsString(message, errs))
+	if logger, enabled := defaultLogger(slog.LevelWarn); enabled {
+		logger.log(message, errorsCauseAttribute(errs))
 	}
 }
 
@@ -77,31 +78,32 @@ func WarnErrors(message string, errs ...error) {
 //
 // If message is not blank, wraps the error with the given message using [hermannm.dev/wrap.Error].
 func Error(err error, message string, attributes ...slog.Attr) {
-	if logger, enabled := getLogger(slog.LevelError); enabled {
-		logger.log(buildErrorString(err, message), attributes...)
+	if logger, enabled := defaultLogger(slog.LevelError); enabled {
+		message, attributes = buildErrorLog(err, message, attributes)
+		logger.log(message, attributes...)
 	}
 }
 
 // Errorf wraps the given error with a formatted message using [hermannm.dev/wrap.Errorf], and logs
 // it at the ERROR log level.
 func Errorf(err error, messageFormat string, formatArgs ...any) {
-	if logger, enabled := getLogger(slog.LevelError); enabled {
-		logger.log(buildWrappedErrorString(err, fmt.Sprintf(messageFormat, formatArgs...)))
+	if logger, enabled := defaultLogger(slog.LevelError); enabled {
+		logger.log(fmt.Sprintf(messageFormat, formatArgs...), errorCauseAttribute(err))
 	}
 }
 
 // Errors wraps the given errors with a message using [hermannm.dev/wrap.Errors], and logs it at the
 // ERROR log level.
 func Errors(message string, errs ...error) {
-	if logger, enabled := getLogger(slog.LevelError); enabled {
-		logger.log(buildWrappedErrorsString(message, errs))
+	if logger, enabled := defaultLogger(slog.LevelError); enabled {
+		logger.log(message, errorsCauseAttribute(errs))
 	}
 }
 
 // ErrorMessage logs the given message at the ERROR log level, along with any given structured log
 // attributes.
 func ErrorMessage(message string, attributes ...slog.Attr) {
-	if logger, enabled := getLogger(slog.LevelError); enabled {
+	if logger, enabled := defaultLogger(slog.LevelError); enabled {
 		logger.log(message, attributes...)
 	}
 }
@@ -109,7 +111,7 @@ func ErrorMessage(message string, attributes ...slog.Attr) {
 // ErrorMessagef creates a message from the given format and arguments using [fmt.Sprintf], and logs
 // it at the ERROR log level.
 func ErrorMessagef(messageFormat string, formatArgs ...any) {
-	if logger, enabled := getLogger(slog.LevelError); enabled {
+	if logger, enabled := defaultLogger(slog.LevelError); enabled {
 		logger.log(fmt.Sprintf(messageFormat, formatArgs...))
 	}
 }
@@ -120,7 +122,7 @@ func ErrorMessagef(messageFormat string, formatArgs ...any) {
 // Note that the DEBUG log level is typically disabled by default in most slog handlers, in which
 // case no output will be produced.
 func Debug(message string, attributes ...slog.Attr) {
-	if logger, enabled := getLogger(slog.LevelDebug); enabled {
+	if logger, enabled := defaultLogger(slog.LevelDebug); enabled {
 		logger.log(message, attributes...)
 	}
 }
@@ -131,56 +133,203 @@ func Debug(message string, attributes ...slog.Attr) {
 // Note that the DEBUG log level is typically disabled by default in most slog handlers, in which
 // case no output will be produced.
 func Debugf(messageFormat string, formatArgs ...any) {
-	if logger, enabled := getLogger(slog.LevelDebug); enabled {
+	if logger, enabled := defaultLogger(slog.LevelDebug); enabled {
 		logger.log(fmt.Sprintf(messageFormat, formatArgs...))
 	}
 }
 
-// DebugJSON serializes the given value to a prettified JSON format, and logs it at the DEBUG log
+// DebugJSON marshals the given value to a prettified JSON format, and logs it at the DEBUG log
 // level, along with any given structured log attributes. If message is not blank, the JSON is
 // prefixed by the message and a colon. Uses colors if [ColorsEnabled] is true.
 //
 // Note that the DEBUG log level is typically disabled by default in most slog handlers, in which
 // case no output will be produced.
 func DebugJSON(value any, message string, attributes ...slog.Attr) {
-	if logger, enabled := getLogger(slog.LevelDebug); enabled {
-		var buffer bytes.Buffer
+	if logger, enabled := defaultLogger(slog.LevelDebug); enabled {
+		logger.log(buildDebugJSONString(value, message), attributes...)
+	}
+}
 
-		encoder := jsoncolor.NewEncoder(&buffer)
-		encoder.SetIndent("", "  ")
+// Logger provides logging methods that produce log records to pass to its output handler.
+// It is analogous to [slog.Logger], but provides more utilities for log message formatting.
+type Logger struct {
+	handler slog.Handler
+}
 
-		if ColorsEnabled {
-			encoder.SetColors(&jsonColors)
+// New creates a new [Logger] to produce log records for the given output handler.
+func New(outputHandler slog.Handler) *Logger {
+	return &Logger{handler: outputHandler}
+}
 
-			if message != "" {
-				buffer.WriteString(message)
-				buffer.Write(jsonColors.Punc)
-				buffer.WriteByte(':')
-				buffer.Write(color.Reset)
-				buffer.WriteByte(' ')
-			}
-		} else {
-			if message != "" {
-				buffer.WriteString(message)
-				buffer.WriteString(": ")
-			}
-		}
+// With returns a Logger that includes the given attributes in each log.
+// If no attributes are given, the logger is returned as-is.
+func (logger *Logger) With(attributes ...slog.Attr) *Logger {
+	if len(attributes) == 0 {
+		return logger
+	}
 
-		err := encoder.Encode(value)
-		if err == nil {
-			bytes := buffer.Bytes()
-			bytes = bytes[0 : len(bytes)-1] // Removes trailing newline
-			logger.log(string(bytes), attributes...)
-		} else {
-			fmt.Fprint(&buffer, value)
-			logger.log(buffer.String(), attributes...)
-		}
+	newLogger := *logger
+	newLogger.handler = newLogger.handler.WithAttrs(attributes)
+	return &newLogger
+}
+
+// WithGroup returns a Logger that starts an attribute group.
+// The keys of attributes added to the Logger (through [Logger.With]) will be qualified by the given
+// name. If name is empty, the logger is returned as-is.
+func (logger *Logger) WithGroup(name string) *Logger {
+	if name == "" {
+		return logger
+	}
+
+	newLogger := *logger
+	newLogger.handler = newLogger.handler.WithGroup(name)
+	return &newLogger
+}
+
+// Handler returns the output handler for the logger.
+func (logger *Logger) Handler() slog.Handler {
+	return logger.handler
+}
+
+// Info logs the given message at the INFO log level, along with any given structured log
+// attributes.
+func (logger *Logger) Info(message string, attributes ...slog.Attr) {
+	if level, enabled := logger.withLevel(slog.LevelInfo); enabled {
+		level.log(message, attributes...)
+	}
+}
+
+// Infof creates a message from the given format and arguments using [fmt.Sprintf], and logs it at
+// the INFO log level.
+func (logger *Logger) Infof(messageFormat string, formatArgs ...any) {
+	if level, enabled := logger.withLevel(slog.LevelInfo); enabled {
+		level.log(fmt.Sprintf(messageFormat, formatArgs...))
+	}
+}
+
+// Warn logs the given message at the WARN log level, along with any given structured log
+// attributes.
+func (logger *Logger) Warn(message string, attributes ...slog.Attr) {
+	if level, enabled := logger.withLevel(slog.LevelWarn); enabled {
+		level.log(message, attributes...)
+	}
+}
+
+// Warnf creates a message from the given format and arguments using [fmt.Sprintf], and logs it at
+// the WARN log level.
+func (logger *Logger) Warnf(messageFormat string, formatArgs ...any) {
+	if level, enabled := logger.withLevel(slog.LevelWarn); enabled {
+		level.log(fmt.Sprintf(messageFormat, formatArgs...))
+	}
+}
+
+// WarnError logs the given error at the WARN log level, along with any given structured log
+// attributes.
+//
+// If message is not blank, wraps the error with the given message using [hermannm.dev/wrap.Error].
+func (logger *Logger) WarnError(err error, message string, attributes ...slog.Attr) {
+	if level, enabled := logger.withLevel(slog.LevelWarn); enabled {
+		message, attributes = buildErrorLog(err, message, attributes)
+		level.log(message, attributes...)
+	}
+}
+
+// WarnErrorf wraps the given error with a formatted message using [hermannm.dev/wrap.Errorf], and
+// logs it at the WARN log level.
+func (logger *Logger) WarnErrorf(err error, messageFormat string, formatArgs ...any) {
+	if level, enabled := logger.withLevel(slog.LevelWarn); enabled {
+		level.log(fmt.Sprintf(messageFormat, formatArgs...), errorCauseAttribute(err))
+	}
+}
+
+// WarnErrors wraps the given errors with a message using [hermannm.dev/wrap.Errors], and logs it at
+// the WARN log level.
+func (logger *Logger) WarnErrors(message string, errs ...error) {
+	if level, enabled := logger.withLevel(slog.LevelWarn); enabled {
+		level.log(message, errorsCauseAttribute(errs))
+	}
+}
+
+// Error logs the given error at the ERROR log level, along with any given structured log
+// attributes.
+//
+// If message is not blank, wraps the error with the given message using [hermannm.dev/wrap.Error].
+func (logger *Logger) Error(err error, message string, attributes ...slog.Attr) {
+	if level, enabled := logger.withLevel(slog.LevelError); enabled {
+		message, attributes = buildErrorLog(err, message, attributes)
+		level.log(message, attributes...)
+	}
+}
+
+// Errorf wraps the given error with a formatted message using [hermannm.dev/wrap.Errorf], and logs
+// it at the ERROR log level.
+func (logger *Logger) Errorf(err error, messageFormat string, formatArgs ...any) {
+	if level, enabled := logger.withLevel(slog.LevelError); enabled {
+		level.log(fmt.Sprintf(messageFormat, formatArgs...), errorCauseAttribute(err))
+	}
+}
+
+// Errors wraps the given errors with a message using [hermannm.dev/wrap.Errors], and logs it at the
+// ERROR log level.
+func (logger *Logger) Errors(message string, errs ...error) {
+	if level, enabled := logger.withLevel(slog.LevelError); enabled {
+		level.log(message, errorsCauseAttribute(errs))
+	}
+}
+
+// ErrorMessage logs the given message at the ERROR log level, along with any given structured log
+// attributes.
+func (logger *Logger) ErrorMessage(message string, attributes ...slog.Attr) {
+	if level, enabled := logger.withLevel(slog.LevelError); enabled {
+		level.log(message, attributes...)
+	}
+}
+
+// ErrorMessagef creates a message from the given format and arguments using [fmt.Sprintf], and logs
+// it at the ERROR log level.
+func (logger *Logger) ErrorMessagef(messageFormat string, formatArgs ...any) {
+	if level, enabled := logger.withLevel(slog.LevelError); enabled {
+		level.log(fmt.Sprintf(messageFormat, formatArgs...))
+	}
+}
+
+// Debug logs the given message at the DEBUG log level, along with any given structured log
+// attributes.
+//
+// Note that the DEBUG log level is typically disabled by default in most slog handlers, in which
+// case no output will be produced.
+func (logger *Logger) Debug(message string, attributes ...slog.Attr) {
+	if level, enabled := logger.withLevel(slog.LevelDebug); enabled {
+		level.log(message, attributes...)
+	}
+}
+
+// Debugf creates a message from the given format and arguments using [fmt.Sprintf], and logs it at
+// the DEBUG log level.
+//
+// Note that the DEBUG log level is typically disabled by default in most slog handlers, in which
+// case no output will be produced.
+func (logger *Logger) Debugf(messageFormat string, formatArgs ...any) {
+	if level, enabled := logger.withLevel(slog.LevelDebug); enabled {
+		level.log(fmt.Sprintf(messageFormat, formatArgs...))
+	}
+}
+
+// DebugJSON marshals the given value to a prettified JSON format, and logs it at the DEBUG log
+// level, along with any given structured log attributes. If message is not blank, the JSON is
+// prefixed by the message and a colon. Uses colors if [ColorsEnabled] is true.
+//
+// Note that the DEBUG log level is typically disabled by default in most slog handlers, in which
+// case no output will be produced.
+func (logger *Logger) DebugJSON(value any, message string, attributes ...slog.Attr) {
+	if level, enabled := logger.withLevel(slog.LevelDebug); enabled {
+		level.log(buildDebugJSONString(value, message), attributes...)
 	}
 }
 
 type levelLogger struct {
-	*slog.Logger
-	level slog.Level
+	handler slog.Handler
+	level   slog.Level
 }
 
 // Follows the example from the slog package for how to properly wrap its functions:
@@ -196,11 +345,48 @@ func (logger levelLogger) log(message string, attributes ...slog.Attr) {
 	record := slog.NewRecord(time.Now(), logger.level, message, programCounters[0])
 	record.AddAttrs(attributes...)
 
-	_ = logger.Handler().Handle(context.Background(), record)
+	_ = logger.handler.Handle(context.Background(), record)
 }
 
-func getLogger(level slog.Level) (logger levelLogger, enabled bool) {
-	logger = levelLogger{Logger: slog.Default(), level: level}
+func defaultLogger(level slog.Level) (logger levelLogger, enabled bool) {
+	logger = levelLogger{handler: slog.Default().Handler(), level: level}
+	return logger, logger.handler.Enabled(context.Background(), logger.level)
+}
 
-	return logger, logger.Enabled(context.Background(), logger.level)
+func (logger *Logger) withLevel(level slog.Level) (withLevel levelLogger, enabled bool) {
+	return levelLogger{handler: logger.handler, level: level},
+		logger.handler.Enabled(context.Background(), level)
+}
+
+func buildDebugJSONString(value any, message string) string {
+	var buffer bytes.Buffer
+	encoder := jsoncolor.NewEncoder(&buffer)
+	encoder.SetIndent("", "  ")
+
+	if ColorsEnabled {
+		encoder.SetColors(&jsonColors)
+
+		if message != "" {
+			buffer.WriteString(message)
+			buffer.Write(jsonColors.Punc)
+			buffer.WriteByte(':')
+			buffer.Write(color.Reset)
+			buffer.WriteByte(' ')
+		}
+	} else {
+		if message != "" {
+			buffer.WriteString(message)
+			buffer.WriteString(": ")
+		}
+	}
+
+	err := encoder.Encode(value)
+	if err == nil {
+		bytes := buffer.Bytes()
+		bytes = bytes[0 : len(bytes)-1] // Removes trailing newline
+		return string(bytes)
+	} else {
+		fmt.Fprint(&buffer, value)
+		return buffer.String()
+	}
 }
