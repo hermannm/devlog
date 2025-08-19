0add208d2468be8a18a488820fe53da1f315f9c8
hermannm <hermann.morkrid@gmail.com>
2025-08-20 00:01:59 +0200

    Add log.AddContextAttrs function for adding structured log attrs to ctx

---
+++ log/context.go
@@ -0,0 +1,40 @@
+package log
+
+import (
+	"context"
+	"log/slog"
+)
+
+func AddContextAttrs(parent context.Context, logAttributes ...any) context.Context {
+	existingAttrs := getContextAttrs(parent)
+	if len(existingAttrs) != 0 {
+		newAttrs := make([]slog.Attr, 0, len(existingAttrs)+len(logAttributes))
+		copy(newAttrs, existingAttrs)
+		newAttrs = parseAttrs(newAttrs, logAttributes)
+		return context.WithValue(parent, contextAttrsKey, newAttrs)
+	}
+
+	attrs := make([]slog.Attr, 0, len(logAttributes))
+	attrs = parseAttrs(attrs, logAttributes)
+	return context.WithValue(parent, contextAttrsKey, attrs)
+}
+
+// Returns nil if there were no attrs in the given context.
+func getContextAttrs(ctx context.Context) []slog.Attr {
+	contextValue := ctx.Value(contextAttrsKey)
+	if contextValue == nil {
+		return nil
+	}
+
+	attrs, ok := contextValue.([]slog.Attr)
+	if !ok {
+		return nil
+	}
+
+	return attrs
+}
+
+// Use struct{} to avoid allocations, as recommended by [context.WithValue].
+type contextAttrsKeyType struct{}
+
+var contextAttrsKey = contextAttrsKeyType{}
